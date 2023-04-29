c8c8ada0af420b30d1dadf9f2331eeafc9793010
9Alla9 <konfetcka@gmail.com>
2023-04-29 20:57:44 +0300

    Update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -5,56 +5,84 @@ import { galleryItems } from './gallery-items';
 
 // Change code below this line
 
+// const galleryContainer = document.querySelector('.gallery');
+// const itemsMarkup = createGalleryItemsMarkup(galleryItems);
+// galleryContainer.insertAdjacentHTML('beforeend', itemsMarkup);
+// galleryContainer.addEventListener('click', onImgClick);
+
+// function createGalleryItemsMarkup(items) {
+//   return items
+//     .map(({ preview, original, description }) => {
+//       return `<div class="gallery__item">
+//   <a class="gallery__link" href="${original}">
+//     <img
+//       class="gallery__image"
+//       src="${preview}"
+//       data-source="${original}"
+//       alt="${description}"
+//     />
+//   </a>
+// </div>`;
+//     })
+//     .join('');
+// }
+
+// function onImgClick(e) {
+//   e.preventDefault();
+
+//   const isItemImage = e.target.classList.contains('gallery__image');
+//   if (!isItemImage) return;
+
+//   const currentImgUrl = e.target.dataset.source;
+
+//   const instance = basicLightbox.create(
+//     `
+// 		<img src="${currentImgUrl}" width="1280" height="auto"/>
+//         `,
+//     {
+//       onShow: instance => {
+//         window.addEventListener('keydown', onEscKeyPress);
+//       },
+//       onClose: instance => {
+//         window.removeEventListener('keydown', onEscKeyPress);
+//       },
+//     }
+//   );
+//   instance.show();
+
+//   function onEscKeyPress(e) {
+//     const ESC_KEY_CODE = 'Escape';
+//     const isEscKey = e.code === ESC_KEY_CODE;
+//     if (!isEscKey) return;
+//     instance.close();
+//   }
+// }
+console.log(galleryItems);
+
 const galleryContainer = document.querySelector('.gallery');
 const itemsMarkup = createGalleryItemsMarkup(galleryItems);
 galleryContainer.insertAdjacentHTML('beforeend', itemsMarkup);
-galleryContainer.addEventListener('click', onImgClick);
 
+// rendered items
 function createGalleryItemsMarkup(items) {
   return items
     .map(({ preview, original, description }) => {
-      return `<div class="gallery__item">
-  <a class="gallery__link" href="${original}">
+      return `
+  <a class="gallery__item" href="${original}">
     <img
       class="gallery__image"
       src="${preview}"
-      data-source="${original}"
       alt="${description}"
     />
   </a>
-</div>`;
+`;
     })
     .join('');
 }
 
-function onImgClick(e) {
-  e.preventDefault();
-
-  const isItemImage = e.target.classList.contains('gallery__image');
-  if (!isItemImage) return;
-
-  const currentImgUrl = e.target.dataset.source;
-
-  const instance = basicLightbox.create(
-    `
-		<img src="${currentImgUrl}" width="1280" height="auto"/>
-        `,
-    {
-      onShow: instance => {
-        window.addEventListener('keydown', onEscKeyPress);
-      },
-      onClose: instance => {
-        window.removeEventListener('keydown', onEscKeyPress);
-      },
-    }
-  );
-  instance.show();
-
-  function onEscKeyPress(e) {
-    const ESC_KEY_CODE = 'Escape';
-    const isEscKey = e.code === ESC_KEY_CODE;
-    if (!isEscKey) return;
-    instance.close();
-  }
-}
-console.log(galleryItems);
+// use library SimpleLightbox
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+});
