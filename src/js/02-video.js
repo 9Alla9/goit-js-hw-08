5c3151f6d0c245faae9d3e14f825705963cdefe6
9Alla9 <konfetcka@gmail.com>
2023-04-30 16:39:38 +0300

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -11,13 +11,29 @@ import throttle from 'lodash.throttle';
   allow="autoplay; encrypted-media"
 ></iframe>;
 
+const CURRENT_TIME_KEY = 'videoplayer-current-time';
+
 const iframe = document.querySelector('iframe');
-const player = new Vimeo.Player(iframe);
-
-player.on('play', function () {
-  console.log('played the video!');
+const player = new Player(iframe, {
+  loop: true,
+  fullscreen: true,
+  quality: '1080p',
 });
 
-player.getVideoTitle().then(function (title) {
-  console.log('title:', title);
-});
+const getCurrentTime = function (currentTime) {
+  const seconds = currentTime.seconds;
+  localStorage.setItem(CURRENT_TIME_KEY, JSON.stringify(seconds));
+};
+
+player.on('timeupdate', throttle(getCurrentTime, 1000));
+
+player.setCurrentTime(JSON.parse(localStorage.getItem(CURRENT_TIME_KEY)) || 0);
+
+player
+  .setColor('#d8e0ff')
+  .then(function (color) {
+    console.log('The new color value: #D8E0FF');
+  })
+  .catch(function (error) {
+    console.log('An error occurred while setting the color');
+  });
