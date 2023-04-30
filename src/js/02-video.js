bef19410738267e17a16244bdd6e42a21c8ce0f6
9Alla9 <konfetcka@gmail.com>
2023-05-01 00:02:16 +0300

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,64 +1,29 @@
-import '../css/common.css';
 import Player from '@vimeo/player';
-import { throttle } from 'lodash';
+import throttle from 'lodash.throttle';
 
-const TIME_KEY = 'videoplayer-current-time';
+const CURRENT_TIME_KEY = 'videoplayer-current-time';
+
 const iframe = document.querySelector('iframe');
-const player = new Vimeo.Player(iframe);
+const player = new Player(iframe, {
+  loop: true,
+  fullscreen: true,
+  quality: '1080p',
+});
 
-const onPlay = function (data) {
-  const strigifyData = JSON.stringify(data);
-  localStorage.setItem(TIME_KEY, strigifyData);
+const getCurrentTime = function (currentTime) {
+  const seconds = currentTime.seconds;
+  localStorage.setItem(CURRENT_TIME_KEY, JSON.stringify(seconds));
 };
-player.on('timeupdate', throttle(onPlay, 1000));
 
-function resumePlayback() {
-  if (JSON.parse(localStorage.getItem(TIME_KEY)) === null) {
-    return;
-  }
-  const paused = JSON.parse(localStorage.getItem(TIME_KEY))['seconds'];
-  if (paused) {
-    player
-      .setCurrentTime(paused)
-      .then(function (seconds) {})
-      .catch(function (error) {
-        switch (error.name) {
-          case 'Error':
-            break;
-          default:
-            break;
-        }
-      });
-  }
-}
-resumePlayback();
+player.on('timeupdate', throttle(getCurrentTime, 1000));
 
-// import Player from '@vimeo/player';
-// import throttle from 'lodash.throttle';
+player.setCurrentTime(JSON.parse(localStorage.getItem(CURRENT_TIME_KEY)) || 0);
 
-// const CURRENT_TIME_KEY = 'videoplayer-current-time';
-
-// const iframe = document.querySelector('iframe');
-// const player = new Player(iframe, {
-//   loop: true,
-//   fullscreen: true,
-//   quality: '1080p',
-// });
-
-// const getCurrentTime = function (currentTime) {
-//   const seconds = currentTime.seconds;
-//   localStorage.setItem(CURRENT_TIME_KEY, JSON.stringify(seconds));
-// };
-
-// player.on('timeupdate', throttle(getCurrentTime, 1000));
-
-// player.setCurrentTime(JSON.parse(localStorage.getItem(CURRENT_TIME_KEY)) || 0);
-
-// player
-//   .setColor('#d8e0ff')
-//   .then(function (color) {
-//     console.log('The new color value: #D8E0FF');
-//   })
-//   .catch(function (error) {
-//     console.log('An error occurred while setting the color');
-//   });
+player
+  .setColor('#d8e0ff')
+  .then(function (color) {
+    console.log('The new color value: #D8E0FF');
+  })
+  .catch(function (error) {
+    console.log('An error occurred while setting the color');
+  });
