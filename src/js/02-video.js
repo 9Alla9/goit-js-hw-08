ce1c12d08e510c1e6f3118b29444f27af4c1f5e6
9Alla9 <konfetcka@gmail.com>
2023-04-30 23:54:22 +0300

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,29 +1,64 @@
+import '../css/common.css';
 import Player from '@vimeo/player';
-import throttle from 'lodash.throttle';
+import { throttle } from 'lodash';
 
-const CURRENT_TIME_KEY = 'videoplayer-current-time';
-
+const TIME_KEY = 'videoplayer-current-time';
 const iframe = document.querySelector('iframe');
-const player = new Player(iframe, {
-  loop: true,
-  fullscreen: true,
-  quality: '1080p',
-});
+const player = new Vimeo.Player(iframe);
 
-const getCurrentTime = function (currentTime) {
-  const seconds = currentTime.seconds;
-  localStorage.setItem(CURRENT_TIME_KEY, JSON.stringify(seconds));
+const onPlay = function (data) {
+  const strigifyData = JSON.stringify(data);
+  localStorage.setItem(TIME_KEY, strigifyData);
 };
+player.on('timeupdate', throttle(onPlay, 1000));
 
-player.on('timeupdate', throttle(getCurrentTime, 1000));
+function resumePlayback() {
+  if (JSON.parse(localStorage.getItem(TIME_KEY)) === null) {
+    return;
+  }
+  const paused = JSON.parse(localStorage.getItem(TIME_KEY))['seconds'];
+  if (paused) {
+    player
+      .setCurrentTime(paused)
+      .then(function (seconds) {})
+      .catch(function (error) {
+        switch (error.name) {
+          case 'Error':
+            break;
+          default:
+            break;
+        }
+      });
+  }
+}
+resumePlayback();
 
-player.setCurrentTime(JSON.parse(localStorage.getItem(CURRENT_TIME_KEY)) || 0);
+// import Player from '@vimeo/player';
+// import throttle from 'lodash.throttle';
 
-player
-  .setColor('#d8e0ff')
-  .then(function (color) {
-    console.log('The new color value: #D8E0FF');
-  })
-  .catch(function (error) {
-    console.log('An error occurred while setting the color');
-  });
+// const CURRENT_TIME_KEY = 'videoplayer-current-time';
+
+// const iframe = document.querySelector('iframe');
+// const player = new Player(iframe, {
+//   loop: true,
+//   fullscreen: true,
+//   quality: '1080p',
+// });
+
+// const getCurrentTime = function (currentTime) {
+//   const seconds = currentTime.seconds;
+//   localStorage.setItem(CURRENT_TIME_KEY, JSON.stringify(seconds));
+// };
+
+// player.on('timeupdate', throttle(getCurrentTime, 1000));
+
+// player.setCurrentTime(JSON.parse(localStorage.getItem(CURRENT_TIME_KEY)) || 0);
+
+// player
+//   .setColor('#d8e0ff')
+//   .then(function (color) {
+//     console.log('The new color value: #D8E0FF');
+//   })
+//   .catch(function (error) {
+//     console.log('An error occurred while setting the color');
+//   });
