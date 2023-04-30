07d39f3c4a7329e9ce960f1b60ec31c777ce5583
9Alla9 <konfetcka@gmail.com>
2023-04-30 23:08:10 +0300

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,28 +1,30 @@
 import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
 
-const CURRENT_TIME_KEY = 'videoplayer-current-time';
 const iframe = document.querySelector('iframe');
-const player = new Player(iframe, {
-  loop: true,
-  fullscreen: true,
-  quality: '1080p',
-});
+const player = new Player(iframe);
 
-const getCurrentTime = function (currentTime) {
-  const seconds = currentTime.seconds;
-  localStorage.setItem(CURRENT_TIME_KEY, JSON.stringify(seconds));
+const onPlay = function (data) {
+  localStorage.setItem('videoplayer-current-time', data.seconds);
 };
 
-player.on('timeupdate', throttle(getCurrentTime, 1000));
+player.on('timeupdate', throttle(onPlay, 1000));
 
-player.setCurrentTime(JSON.parse(localStorage.getItem(CURRENT_TIME_KEY)) || 0);
+const currentTime = Number(localStorage.getItem('videoplayer-current-time'));
 
 player
-  .setColor('#d8e0ff')
-  .then(function (color) {
-    console.log('The new color value: #D8E0FF');
-  })
+  .setCurrentTime(currentTime)
+  .then(function (seconds) {})
   .catch(function (error) {
-    console.log('An error occurred while setting the color');
+    switch (error.name) {
+      case 'RangeError':
+        break;
+      default:
+        break;
+    }
   });
+
+player
+  .setColor('#45a247')
+  .then(function (color) {})
+  .catch(function (error) {});
